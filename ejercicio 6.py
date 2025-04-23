1b9e646560e3514a0816deab8ef25a252334e740
TomasSR_Coder <tomasruiz1104@gmail.com>
2025-04-23 12:23:36 -0500

    Delete ejercicio 6.py

--- ejercicio 6.py
+++
@@ -1,8 +0,0 @@
-es_miembro = True
-total_compra = 120
-
-# Operadores lógicos y not
-if total_compra > 100 and (es_miembro or not total_compra > 200):
-    print("Descuento del 15% aplicado 🎉")
-else:
-    print("Sin descuento 😢")
