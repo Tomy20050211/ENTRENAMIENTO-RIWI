643bf45b1c7d9f59ee9aa3ea9b8a0d4fec4cf787
TomasSR_Coder <tomasruiz1104@gmail.com>
2025-04-23 12:22:53 -0500

    Delete ejercicio 2.py

--- ejercicio 2.py
+++
@@ -1,9 +0,0 @@
-ingreso_mensual = 2500
-deuda = 0
-puntuacion_credito = 700
-
-# Operadores lógicos
-if (ingreso_mensual or puntuacion_credito ) and deuda == 0:
-    print("Préstamo aprobado 💰")
-else:
-    print("Préstamo rechazado 🚫")
